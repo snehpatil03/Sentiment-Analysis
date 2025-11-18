2b9f11d240c18eaf879ad3ec3474c6018f975d46
Sneh Patil <patil.sneh1@northeastern.edu>
2025-11-18 12:38:43 -0500

    Changes in the file

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -85,13 +85,11 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action;
 
-      // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId);
       } else {
         state.toasts.forEach((toast) => {
-          addToRemoveQueue(toast.id);
+          addToRemoveQueue(toast.id)
         });
       }
 

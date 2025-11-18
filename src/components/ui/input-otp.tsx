f9b395de803de56d26e100d2978a848968706c4a
Sneh Patil <patil.sneh1@northeastern.edu>
2025-11-18 15:09:34 -0500

    Sentiment Aura App

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -14,7 +14,7 @@ const InputOTP = React.forwardRef<React.ElementRef<typeof OTPInput>, React.Compo
     />
   ),
 );
-InputOTP.displayName = "InputOTP";
+InputOTP.displayName = "InputOTP"
 
 const InputOTPGroup = React.forwardRef<React.ElementRef<"div">, React.ComponentPropsWithoutRef<"div">>(
   ({ className, ...props }, ref) => <div ref={ref} className={cn("flex items-center", className)} {...props} />,

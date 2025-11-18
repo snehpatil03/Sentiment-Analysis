cb1d9a8748ca1bf267ce5d22ec925f384e732f3a
Sneh Patil <patil.sneh1@northeastern.edu>
2025-11-18 15:01:08 -0500

    Sentiment Aura App

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -49,6 +49,6 @@ function Calendar({ className, classNames, showOutsideDays = true, ...props }: C
     />
   );
 }
-Calendar.displayName = "Calendar";
+Calendar.displayName = "Calendar"
 
 export { Calendar };

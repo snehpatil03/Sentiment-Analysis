80696a4fdf17d67e927e0f906dd3fdaeb8b35918
Sneh Patil <patil.sneh1@northeastern.edu>
2025-11-18 12:42:59 -0500

    Sentiment Aura App

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 const Accordion = AccordionPrimitive.Root;
 
 const AccordionItem = React.forwardRef<
-  React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ElementRef<typeof AccordionPrimitive.Item>
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item ref={ref} className={cn("border-b", className)} {...props} />
